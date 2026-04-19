cf2029dcc611c79c696269d3b664bdafe92cc8bf
Samuel Barasa <samuelbarasa2019@gmail.com>
2026-04-19 07:21:29 +0300

    fix: configure CORS for production

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,18 @@ const app = express()
 const PORT = process.env.PORT || 3002
 
 // Middleware
-app.use(cors())
+app.use(cors(
+  {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://smartseason-five.vercel.app/'  
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+))
 app.use(express.json())
 
 // Initialize database
